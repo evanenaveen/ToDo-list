1d521af1e3d2f8147cf9eab3736ccd2cc35a8f60
Naveen Evane <naveenevanemultai26@gmail.com>
2022-04-08 05:22:53 +0530

    Project done

---
+++ demoproject/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MycomponentTodosComponent } from './mycomponent-todos/mycomponent-todos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MycomponentTodosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
